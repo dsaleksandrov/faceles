232dc9578d54f16980b7e0cf24f757fadc0b79fc
dsaleksandrov <dsaleksandrov@ubuntu.(none)>
2012-08-08 10:26:08 +0400

    sdfsfs

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -34,36 +34,4 @@ class LikesController < ApplicationController
   end
   end
 
-  # GET /likes/1
-  # GET /likes/1.json
-  def show
-    @like = Like.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @like }
-    end
-  end
- 
-
-  # GET /likes/1/edit
-  def edit
-    @like = Like.find(params[:id])
-  end
-
-  # POST /likes
-  # POST /likes.json
-  def create
-    @like = Like.new(params[:like])
-
-    respond_to do |format|
-      if @like.save
-        format.html { redirect_to @like, notice: 'Like was successfully created.' }
-        format.json { render json: @like, status: :created, location: @like }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
  end
